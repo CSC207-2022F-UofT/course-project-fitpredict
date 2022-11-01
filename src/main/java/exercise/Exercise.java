77e2ce6f6d276f832beb92c79227e9e597156b10
penguincookies <jamiesumin@gmail.com>
2022-11-01 01:28:02 -0400

    Exercise entity class created

---
+++ src/main/java/exercise/Exercise.java
@@ -0,0 +1,48 @@
+package exercise;
+
+public class Exercise {
+    String name;
+    String type;
+    int caloriesBurntPerMin;
+
+    /**
+     * Constructor
+     */
+    public Exercise (String name, int caloriesBurntPerMin) {
+        this.name = name;
+        this.caloriesBurntPerMin = caloriesBurntPerMin;
+    }
+
+    /**
+     * Constructor 2
+     */
+    public Exercise (String name, String type, int caloriesBurntPerMin) {
+        this.name = name;
+        this.type = type;
+        this.caloriesBurntPerMin = caloriesBurntPerMin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCaloriesBurntPerMin() {
+        return caloriesBurntPerMin;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCaloriesBurntPerMin(int caloriesBurntPerMin) {
+        this.caloriesBurntPerMin = caloriesBurntPerMin;
+    }
+}
