f5e69b8142da557c472238d0834e2adb0f29291a
selintasman <selintasman14@gmail.com>
2022-11-10 17:55:18 +0000

    The UI for the Login has been created.

---
+++ src/main/java/user_login/LoginScreenMain.java
@@ -0,0 +1,11 @@
+package user_login;
+
+
+public class LoginScreenMain {
+    public static void main(String[] args) {
+        UserLoginScreen screen = new UserLoginScreen();
+        screen.setSize(300, 300);
+        screen.setVisible(true);
+
+    }
+}
--- src/main/java/user_login/UserLoginScreen.java
+++ src/main/java/user_login/UserLoginScreen.java
@@ -4,12 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-// This is for the GUI.
+
 public class UserLoginScreen extends JFrame implements ActionListener {
     JTextField username = new JTextField(15);
     JPasswordField password = new JPasswordField(15);
 
+    //UserLoginController userLoginController;
+
     public UserLoginScreen() {
+        //this.userLoginController = userLoginController;
         JLabel title = new JLabel("Login Screen");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
         LabelTextPanel usernameInfo = new LabelTextPanel(new JLabel("Username"), username);
@@ -19,6 +22,19 @@ public class UserLoginScreen extends JFrame implements ActionListener {
          JPanel button = new JPanel();
          button.add(logIn);
 
-    }
+         logIn.addActionListener(this);
 
+         JPanel main = new JPanel();
+         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+         main.add(title);
+         main.add(usernameInfo);
+         main.add(passwordInfo);
+         main.add(button);
+         this.setContentPane(main);
+         this.pack();
+
+    }
+    public void actionPerformed(ActionEvent event) {
+        System.out.println("Click" + event.getActionCommand());
+    }
 }
