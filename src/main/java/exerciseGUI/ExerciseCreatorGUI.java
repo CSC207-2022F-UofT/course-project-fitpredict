79ceaa89c11499da8a34adcd0cfcca8afea557eb
penguinc00kies <jamiesumin@gmail.com>
2022-11-12 03:32:49 -0500

    ECG modified back to default

--- src/main/java/exerciseGUI/ExerciseCreatorGUI.java
+++ src/main/java/exerciseGUI/ExerciseCreatorGUI.java
@@ -8,26 +8,26 @@ public class ExerciseCreatorGUI extends JFrame {
     private JLabel label1;
     private JLabel label2;
 
-//    public ExerciseCreatorGUI() {
-//        button1.addActionListener(new ActionListener() {
-//            /**
-//             * Invoked when an action occurs.
-//             *
-//             * @param e the event to be processed
-//             */
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
-//    }
+    public ExerciseCreatorGUI() {
+        button1.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         ExerciseCreatorGUI ecg = new ExerciseCreatorGUI();
-        System.out.println(new ExerciseCreatorGUI().mainPanel.contains(0, 0));
-        // ecg.setContentPane(new ExerciseCreatorGUI().mainPanel);
+        ecg.setContentPane(new ExerciseCreatorGUI().mainPanel);
         ecg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ecg.setVisible(true);
         ecg.pack();
+        ecg.setVisible(true);
+
     }
 }
