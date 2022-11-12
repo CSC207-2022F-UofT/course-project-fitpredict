691b937648386dec4f96c22d67e80208bd08eda4
selintasman <selintasman14@gmail.com>
2022-11-12 23:28:30 +0000

    A controller for user creation has been created.

---
+++ src/main/java/user_creator/UserCreationController.java
@@ -0,0 +1,20 @@
+package user_creator;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class UserCreationController {
+    private final UserCreator userCreator;
+
+    public UserCreationController(UserCreator userCreator) {
+        this.userCreator = userCreator;
+    }
+
+    public ArrayList<String> accountCreator(String username, String password, String repeatPassword,
+                                            Float height, Float weight, String sex, LocalDateTime birthday) {
+        return userCreator.accountCreator(username, password, repeatPassword, height, weight, sex, birthday);
+    }
+
+
+
+}
