f397d40fb13a3ba578a2d72d06c2358ebcd19f10
penguincookies <jamiesumin@gmail.com>
2022-11-12 02:13:02 -0500

    Exercise Creation GUI created

---
+++ src/main/java/exerciseGUI/ExerciseCreatorGUI.java
@@ -0,0 +1,32 @@
+package exerciseGUI;
+
+import javax.swing.*;
+
+public class ExerciseCreatorGUI extends JFrame {
+    private JButton button1;
+    private JPanel mainPanel;
+    private JLabel label1;
+    private JLabel label2;
+
+//    public ExerciseCreatorGUI() {
+//        button1.addActionListener(new ActionListener() {
+//            /**
+//             * Invoked when an action occurs.
+//             *
+//             * @param e the event to be processed
+//             */
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//
+//            }
+//        });
+//    }
+
+    public static void main(String[] args) {
+        ExerciseCreatorGUI ecg = new ExerciseCreatorGUI();
+        ecg.setContentPane(ecg.mainPanel);
+        ecg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ecg.setVisible(true);
+        ecg.pack();
+    }
+}
