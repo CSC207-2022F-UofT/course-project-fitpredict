61b30554dbd9b6ecdfc2d5ab39da06c4075a2812
penguincookies <jamiesumin@gmail.com>
2022-11-12 20:37:20 -0500

    ExerciseMap created, addExercise method implemented

---
+++ src/main/java/exercise/ExerciseMap.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ExerciseList {
+    private final HashMap<String, ArrayList<Exercise>> exerciseMap = new HashMap<String, ArrayList<Exercise>>();
+
+    /**
+     * Adds a new exercise to the current user's ArrayList of exercises
+     * Returns the new exercise added
+     */
+    public Exercise addExercise (User currentUser, Exercise newExercise) {
+        String name = currentUser.getName();
+        if (exerciseMap.containsKey(name)) {
+            exerciseMap.get(name).add(newExercise);
+        }
+        else {
+            exerciseMap.put(name, new ArrayList<Exercise>());
+            exerciseMap.get(name).add(newExercise);
+        }
+        return newExercise
+    }
+}
