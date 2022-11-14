fefdf64773837f6e9b6ab0bee351cf94e6abf27a
elsiezhu <elsie.zhu2003@gmail.com>
2022-11-14 15:05:19 -0500

    add docstrings for all functions in DataPoint and DataPointMap classes

--- src/main/java/DisplayZoom/DataPoint.java
+++ src/main/java/DisplayZoom/DataPoint.java
@@ -17,31 +17,54 @@ public class DataPoint {
     public DataPoint() {
     }
 
+    /*
+    Helper function that is used by a constructor
+    Converts an input of (month, day, year) to milliseconds since Epoch
+    This is only necessary because other Date constructors are deprecated
+     */
     public long convertEpochMilliseconds(int month, int day, int year) {
         LocalDateTime time = LocalDateTime.of(year, month, day, 0, 0);
         return time.toEpochSecond(ZoneOffset.UTC);
     }
 
+    /*
+    Getter function that returns the date of the DataPoint
+     */
     public Date getDate() {
         return this.date;
     }
 
+    /*
+    Getter function that returns the exercises stored in the DataPoint
+     */
     public ArrayList<Exercise> getExerciseList() {
         return this.exerciseList;
     }
 
+    /*
+    Getter function that returns the number of calories burnt that is stored in the DataPoint
+    */
     public float getCaloriesBurnt() {
         return this.caloriesBurnt;
     }
 
+    /*
+    Setter function that sets the number of calories burnt in the DataPoint
+     */
     public void setCaloriesBurnt(float calories) {
         this.caloriesBurnt = calories;
     }
 
+    /*
+    Getter function that returns the user's weight stored in the DataPoint
+     */
     public float getWeight() {
         return this.weight;
     }
 
+    /*
+    Setter function that sets the weight of the user stored in the DataPoint
+     */
     public void setWeight(float weight) {
         this.weight = weight;
     }
--- src/main/java/DisplayZoom/DataPointMap.java
+++ src/main/java/DisplayZoom/DataPointMap.java
@@ -10,10 +10,17 @@ public class DataPointMap {
     public DataPointMap(ArrayList<DataPoint> dataPoints) {
     }
 
+    /*
+    Getter function that returns the HashMap of Date objects mapped to their DataPoint objects
+     */
     public HashMap<Date, DataPoint> getData() {
         return this.dataPointMap;
     }
 
+    /*
+    Getter function that returns the DataPoint object in the DataPointMap with the corresponding Date
+    returns null if a DataPoint with that corresponding date does not exist in the DataPointMap
+     */
     public DataPoint getDataPoint(Date date) {
         for (Date d: this.getData().keySet()) {
             if (d.compareTo(date) == 0) { // if the dates are the same, then compareTo() returns 0
