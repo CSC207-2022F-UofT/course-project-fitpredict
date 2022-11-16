eaec2e999ac1af7eab2392f0ec3497331275ebe1
VangoCode <ronvarshavsky1@gmail.com>
2022-11-16 18:23:04 -0500

    created skeleton functions of prediction feature

---
+++ src/main/java/predictor/CaloriePredictor.java
@@ -0,0 +1,18 @@
+package predictor;
+
+/*
+    Use Case - A class to predict calories burnt over time
+*/
+public class CaloriePredictor {
+
+    /*
+        Returns an average of the calories burnt over the entire DataPointMap
+    */
+    public static float calculateCalories(DataPointMap data) {
+
+        float prediction = 0.0f;
+
+        return prediction;
+    }
+
+}
