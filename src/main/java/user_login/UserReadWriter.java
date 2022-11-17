a257667b3d275beafd420f54a6eccb24df0e0f19
dinalojpur <dinalojpur@gmail.com>
2022-11-16 22:26:52 -0500

    UserReadWriter docstrings have been added to user_login.

--- src/main/java/user_login/UserReadWriter.java
+++ src/main/java/user_login/UserReadWriter.java
@@ -4,10 +4,24 @@ import java.io.IOException;
 
 public class UserReadWriter implements ReadWriter{
 
+    /**
+     * Writes the users to file at filePath.
+     * @param filepath The filepath
+     * @param users The users
+     * @throws IOException Throws exception
+     */
     @Override
-    public void saveToFile(String filepath, Object obj) throws IOException {
+    public void saveToFile(String filepath, Object users) throws IOException {
 
     }
+
+    /**
+     * Reads the users from the file at filepath
+     * @param filepath The filepath
+     * @return the users in the filepath
+     * @throws IOException Throws exception
+     * @throws ClassNotFoundException Throws class not found exception
+     */
 
     public UserAccountList readFromFile(String filepath) throws IOException, ClassNotFoundException {
         return users;
