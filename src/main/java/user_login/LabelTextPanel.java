f456511e06887a7c53009bd48e13e98ab4329f7b
selintasman <selintasman14@gmail.com>
2022-11-16 22:21:28 -0500

    A comment was added.

--- src/main/java/user_login/LabelTextPanel.java
+++ src/main/java/user_login/LabelTextPanel.java
@@ -2,6 +2,7 @@ package user_login;
 
 import javax.swing.*;
 
+// This is for the UI.
 public class LabelTextPanel extends JPanel {
     public LabelTextPanel (JLabel label, JTextField textField) {
         this.add(label);
