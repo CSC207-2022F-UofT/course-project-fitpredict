bb990ca06357fa33217e989a146746c8b46e3ca8
selintasman <selintasman14@gmail.com>
2022-11-16 20:23:17 -0500

    Docstrings were added for the User entity.

--- src/main/java/user_login/User.java
+++ src/main/java/user_login/User.java
@@ -1,10 +1,13 @@
 package user_login;
 
-import java.time.LocalDateTime;
+i
 import java.io.Serializable;
 
 // This is our User entity.
 
+/**
+ * Creates a User entity.
+ */
 public class User implements Serializable {
 
         private String username;
@@ -13,79 +16,141 @@ public class User implements Serializable {
         private Float weight;
         private Float height;
         private String sex;
-        private final LocalDateTime birthday;
+        private final String birthday;
 
-        public User(String username, String password,String repeatPassword,  Float weight, Float height, String sex,
-                    LocalDateTime birthday) {
+    /**
+     *
+     * @param username The user's username
+     * @param password The user's password
+     * @param repeatPassword  The user's repeated password
+     * @param weight The user's weight
+     * @param height The user's height
+     * @param sex The user's sex
+     * @param birthday The user's birthday
+     */
+    public User(String username, String password,String repeatPassword,  Float weight, Float height, String sex,
+                    String birthday) {
+        this.username = username;
+        this.password = password;
+        this.weight = weight;
+        this.height = height;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.repeatPassword = repeatPassword;
+    }
+
+    /**
+     * Returns the user's username.
+     * @return username
+     */
+    public String getUsername(){
+        return username;
+    }
+
+    /**
+     * Sets the User's username.
+     * @param username
+     */
+    public void setUsername(String username){
             this.username = username;
-            this.password = password;
-            this.weight = weight;
-            this.height = height;
-            this.sex = sex;
-            this.birthday = birthday;
-            this.repeatPassword = repeatPassword;
         }
 
-        public String getUsername(){
-            return username;
-        }
-
-        public void setUsername(String username){
-            this.username = username;
-        }
-
-        public Float getWeight(){
+    /**
+     * Returns the user's weight.
+     * @return weight
+     */
+    public Float getWeight(){
             return weight;
         }
 
-        public void setWeight(Float weight) {
-            this.weight = weight;
-        }
+    /**
+     * Sets the User's weight.
+     * @param weight
+     */
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
 
-        public Float getHeight(){
+    /**
+     * Returns the user's height.
+     * @return height.
+     */
+    public Float getHeight(){
             return height;
         }
 
-        public void setHeight(Float height) {
+    /**
+     * Sets the user's height
+     * @param height
+     */
+    public void setHeight(Float height) {
             this.height = height;
         }
 
-        public String getSex(){
+    /**
+     * Returns the user's sex
+     * @return sex
+     */
+    public String getSex(){
             return sex;
         }
 
-        public void setSex(String sex) {
+    /**
+     * Sets the user's sex.
+     * @param sex
+     */
+    public void setSex(String sex) {
             this.sex = sex;
         }
 
-        public LocalDateTime getBirthday(){
+    /**
+     * Returns the User's birthday
+     * @return birthday
+     */
+    public String getBirthday(){
             return birthday;
         }
 
-        public String getPassword() {
+    /**
+     * Return the user's password.
+     * @return password.
+     */
+    public String getPassword() {
             return password;
         }
 
-        public void setPassword(String password) {
+    /**
+     *  Sets the user's password.
+     * @param password
+     */
+    public void setPassword(String password) {
             this.password = password;
         }
 
-        public String getRepeatPassword() {
+    /**
+     * Returns the user's repeated password.
+     * @return repeated password
+     */
+    public String getRepeatPassword() {
             return repeatPassword;
         }
 
-        public void setRepeatPassword(String repeatPassword) {
+    /**
+     * Sets the user's repeated password.
+     * @param repeatPassword
+     */
+    public void setRepeatPassword(String repeatPassword) {
             this.password = repeatPassword;
         }
 
-        public boolean checkPasswordMatches(String password) {
+    /**
+     * Returns true if the two passwrods match
+     * @param password
+     * @return if the two passwrod smatch
+     */
+    public boolean checkPasswordMatches(String password) {
             return this.password.equals(password);
 
-        }
-
-        // Don't we also need a checkUsername method?
-        public boolean checkUsernameMatches(String username) {
-            return this.username.equals(username);
         }
 
 
