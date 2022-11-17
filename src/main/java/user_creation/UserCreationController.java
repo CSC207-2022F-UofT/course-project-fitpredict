71343b83fe16e4eb70d20f914ca75f8b3eb7f37f
dinalojpur <dinalojpur@gmail.com>
2022-11-16 22:55:07 -0500

    UserCreationController class has been added to the skeleton branch.

---
+++ src/main/java/user_creation/UserCreationController.java
@@ -0,0 +1,30 @@
+package user_creation;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class UserCreationController {
+    private final UserCreator userCreator;
+
+    /**
+     * A creation controller for the use case user-creation to use
+     */
+    public UserCreationController(UserCreator userCreator) {
+    }
+
+    /**
+     * Takes the user input, creates an account, and returns an account creation message
+     * @param username The user's username
+     * @param password The user's password
+     * @param repeatPassword The user's repeat password
+     * @param height The user's height
+     * @param weight THe user's weight
+     * @param sex The user's sex
+     * @param birthday The user's birthday
+     * @return if all user input is valid
+     */
+    public ArrayList<String> accountCreator(String username, String password, String repeatPassword,
+                                            Float height, Float weight, String sex, String birthday) {
+        return userCreator.accountCreator(username, password, repeatPassword, height, weight, sex, birthday);
+    }
+}
