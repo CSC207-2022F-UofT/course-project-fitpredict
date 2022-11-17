36d35df37ab67f47b5cc8966d3aaa4c724b56a93
selintasman <selintasman14@gmail.com>
2022-11-17 00:08:44 -0500

    Docstrings were added for all files.

--- src/main/java/user_login/LabelTextPanel.java
+++ src/main/java/user_login/LabelTextPanel.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 public class LabelTextPanel extends JPanel {
 
+    /**
+     * A helper method to create a panel with a label and a text box.
+     */
     public LabelTextPanel (JLabel label, JTextField textField) {
 
     }
