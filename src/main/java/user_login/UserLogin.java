50b20323e2292e4b9bd425d333d00e3ad964f73e
selintasman <selintasman14@gmail.com>
2022-11-16 23:53:52 -0500

    Docstrings were added to UserLogin file.

--- src/main/java/user_login/UserLogin.java
+++ src/main/java/user_login/UserLogin.java
@@ -2,10 +2,19 @@ package user_login;
 
 public class UserLogin {
 
+    /**
+     * A list of users created.
+     */
     private UserAccountList users;
     private User currentUser;
     //UserReadWriter userReadWriter = new UserReadWriter();
 
+    /**
+     * Takes the user's username and password and logs them in if the passwords match.
+     * @param username The user's username.
+     * @param password The user's password.
+     * @return whether the inputted password matches the password linked to that username. .
+     */
     public User loginUser(String username, String password) {
         User user = users.getUser(username);
         if (!user.checkPasswordMatches(password)) {
@@ -15,6 +24,10 @@ public class UserLogin {
         return user;
     }
 
+    /**
+     * Returns the current user that is logged in.
+     * @return the current user.
+     */
     public User getCurrentUser() {
         return this.currentUser;
     }
