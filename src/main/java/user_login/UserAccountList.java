f1f3100ad40263b5eb5136d2db6a0856063be8d3
selintasman <selintasman14@gmail.com>
2022-11-16 19:49:40 -0500

    Docstrings were added for UserAccountList.

--- src/main/java/user_login/UserAccountList.java
+++ src/main/java/user_login/UserAccountList.java
@@ -2,11 +2,25 @@ package user_login;
 
 import java.io.Serializable;
 
+
+
+/**
+ * Creates a map of users
+ */
 public class UserAccountList implements Serializable {
 
+    /**
+     * Adds a new User to the existing list of users
+     * @param NewUser is the user to add to the list
+     */
     public void addNewUser(User NewUser){
     }
 
+    /**
+     * Returns the user's username
+     * @param username is the username of the user
+     * @return if the username exists in the existing list of users
+     */
     public User getUser(String username){
         return UserAccountList.get(username);
 
