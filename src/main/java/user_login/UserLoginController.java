0a80607df7c58768a74c64e0933a6f1bab611aee
selintasman <selintasman14@gmail.com>
2022-11-16 23:44:14 -0500

    Docstrings were added in UserLoginController.

--- src/main/java/user_login/UserLoginController.java
+++ src/main/java/user_login/UserLoginController.java
@@ -3,10 +3,18 @@ package user_login;
 public class UserLoginController {
     private final UserLogin userLogin;
 
+    /**
+     * A Login Controller for the use case UserLogin to use.
+     */
     public UserLoginController(UserLogin userLogin) {
         this.userLogin = userLogin;
     }
 
+    /**
+     * Takes the user's username and password and logs them in.
+     * @param username The user's username.
+     * @param password The user's password.
+     */
     public void login(String username, String password) {
         userLogin.loginUser(username, password);
     }
