a0e67fd985e2767921689824093a6606385d7287
selintasman <selintasman14@gmail.com>
2022-11-16 23:16:00 -0500

    Docstrings were added for UserLoginController

--- src/main/java/user_login/UserLoginController.java
+++ src/main/java/user_login/UserLoginController.java
@@ -2,12 +2,18 @@ package user_login;
 
 public class UserLoginController {
 
+    /**
+     * A Login Controller for the use case UserLogin to use.
+     */
     public UserLoginController(UserLogin userLogin){
     }
+
+    /**
+     * Takes the user's username and password and logs them in.
+     * @param username The user's username.
+     * @param password The user's password.
+     */
     public void login(String username, String password){
 
     }
-
-
-
 }
