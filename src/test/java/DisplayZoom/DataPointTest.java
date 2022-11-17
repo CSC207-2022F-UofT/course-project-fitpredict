8bdfec9dcdabc5fe532559eb23cf31bb6c60bb4a
elsiezhu <elsie.zhu2003@gmail.com>
2022-11-16 19:12:23 -0500

    add empty skeleton tests for DataPoint and DataPointMap

---
+++ src/test/java/DisplayZoom/DataPointTest.java
@@ -0,0 +1,54 @@
+/*
+ * This file contains JUnit test cases for DataPoint.java
+ */
+
+package DisplayZoom;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.io.*;
+
+public class DataPointTest {
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test(timeout = 50)
+    public void testConvertEpochMilliseconds() {
+
+    }
+
+    @Test(timeout = 50)
+    public void testGetDate() {
+
+    }
+
+    @Test(timeout = 50)
+    public void testGetExerciseList() {
+
+    }
+
+    @Test(timeout = 50)
+    public void testGetCaloriesBurnt() {
+
+    }
+
+    @Test(timeout = 50)
+    public void testSetCaloriesBurnt() {
+
+    }
+
+    @Test(timeout = 50)
+    public void testGetWeight() {
+
+    }
+
+    @Test(timeout = 50)
+    public void testSetWeight() {
+
+    }
+
+}
