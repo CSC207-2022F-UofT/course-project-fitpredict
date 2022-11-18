be36ab7f53c03a83fe9b8aea638c742eebfc8b49
VangoCode <ronvarshavsky1@gmail.com>
2022-11-18 15:37:59 -0500

    added buttons to UI for dashboard

--- src/main/java/DisplayView/DashboardScreen.java
+++ src/main/java/DisplayView/DashboardScreen.java
@@ -5,24 +5,39 @@ import java.awt.*;
 
 public class DashboardScreen extends JPanel {
 
+    // Placeholder data for the table. We will input real data after merging
+    String[] columnNames = {"Weight", "Age"};
+    Object[][] data = {
+            {100, 18},
+            {120, 19}
+    };
+
     public DashboardScreen() {
+        // Setting the title and alignment
         JLabel title = new JLabel("Dashboard");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        String[] columnNames = {"Weight", "Age"};
-        Object[][] data = {
-                {100, 18},
-                {120, 19}
-        };
-
+        // The table that will be displayed as part of the dashboard
         JTable table = new JTable(data, columnNames);
 
-        JScrollPane scrollPane = new JScrollPane(table);
+        // Putting the table in a JScrollPane object so it will be fully visible
+        JScrollPane tableScrollPane = new JScrollPane(table);
         table.setFillsViewportHeight(true);
 
+        // Creating the buttons for actions
+        JButton logOut = new JButton("Log out");
+        JButton back = new JButton("Back");
+
+        JPanel buttons = new JPanel();
+        buttons.add(back);
+        buttons.add(logOut);
+
+        // setting the layout of the screen
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        this.add(scrollPane);
+        // adding all the components via JSwing
+        this.add(tableScrollPane);
+        this.add(buttons);
 
     }
 
