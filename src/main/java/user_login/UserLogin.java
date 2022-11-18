e4b5a1a37ede8bfe7e3f67c5068cd109950d043e
selintasman <selintasman14@gmail.com>
2022-11-17 20:02:29 -0500

    UserLogin was modified.

--- src/main/java/user_login/UserLogin.java
+++ src/main/java/user_login/UserLogin.java
@@ -1,7 +1,7 @@
 package user_login;
 
 
-public class UserLogin implements LoginInputBoundary{
+public class UserLogin {
 
     /**
      * A list of users created.
@@ -10,18 +10,21 @@ public class UserLogin implements LoginInputBoundary{
     private CurrentUser currentUser;
     //UserReadWriter userReadWriter = new UserReadWriter();
 
+    public enum LogInResult{
+        LOG, NO_LOG
+    }
     /**
      * Takes the user's username and password and logs them in if the passwords match.
      * @param username The user's username.
      * @param password The user's password.
      * @return whether the inputted password matches the password linked to that username. .
      */
-    public CurrentUser loginUser(String username, String password) {
+    public LogInResult loginUser(String username, String password) {
         User user = users.getUser(username);
         if (!user.checkPasswordMatches(password)) {
-            System.out.println("The passwords don't match.");
+            return LogInResult.NO_LOG;
         }
         currentUser.setUser(user);
-        return currentUser; // This is the user that is currently logged in.
+        return LogInResult.LOG; // This is the user that is currently logged in.
     }
 }
