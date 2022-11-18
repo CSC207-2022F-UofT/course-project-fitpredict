9642aadbdf6336013340465de46a7caa8b9146d2
VangoCode <ronvarshavsky1@gmail.com>
2022-11-18 17:20:53 -0500

    removed unused import

--- src/main/java/DisplayView/DashboardScreen.java
+++ src/main/java/DisplayView/DashboardScreen.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Objects;
 
 public class DashboardScreen extends JPanel implements ActionListener {
 
