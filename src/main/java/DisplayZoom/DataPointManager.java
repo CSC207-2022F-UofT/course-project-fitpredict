33d17a04d2df2ae0fa615749c20577bf2fcdbffb
elsiezhu <elsie.zhu2003@gmail.com>
2022-11-19 18:36:09 -0500

    create helper function in DataPointManager to throw an exception if input is not valid

--- src/main/java/DisplayZoom/DataPointManager.java
+++ src/main/java/DisplayZoom/DataPointManager.java
@@ -8,6 +8,15 @@ public class DataPointManager {
     initialize a DataPoint object with the date (month, day, year)
      */
     public DataPoint createDataPoint(int month, int day, int year) {
+        try {
+            checkValidInput(month, day, year);
+        } catch(IllegalArgumentException e){
+            throw new IllegalArgumentException(e.getMessage());
+        }
+        return new DataPoint(month, day, year);
+    }
+
+    public void checkValidInput(int month, int day, int year) {
         // raise IllegalArgumentException if month is invalid
         if (month > 12 || month < 1) {
             throw new IllegalArgumentException ("A valid calendar month must be between 1-12, inclusive");
@@ -26,7 +35,5 @@ public class DataPointManager {
         if (year < 1) {
             throw new IllegalArgumentException ("A valid calendar year must be greater than or equal to 1");
         }
-
-        return new DataPoint(month, day, year);
     }
 }
