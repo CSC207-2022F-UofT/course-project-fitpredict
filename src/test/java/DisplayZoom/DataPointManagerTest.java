60e855d37959822f194abd55611b2b300aab6d61
elsiezhu <elsie.zhu2003@gmail.com>
2022-11-18 22:57:57 -0500

    create DataPointManager test suite

---
+++ src/test/java/DisplayZoom/DataPointManagerTest.java
@@ -0,0 +1,21 @@
+/*
+ * This file contains JUnit test cases for DataPointManagerTest.java
+ */
+
+package DisplayZoom;
+import org.junit.*;
+
+public class DataPointManagerTest {
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test(timeout = 50)
+    public void testCreateDataPoint() {
+
+    }
+}
