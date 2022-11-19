11e98c71cc391907fbe135c9ab406e2a8396b33e
elsiezhu <elsie.zhu2003@gmail.com>
2022-11-18 22:46:47 -0500

    remove removeDataPoint() method and add class docstring

--- src/main/java/DisplayZoom/DataPointManager.java
+++ src/main/java/DisplayZoom/DataPointManager.java
@@ -1,17 +1,13 @@
 package DisplayZoom;
 
+/*
+DataPointManager class
+ */
 public class DataPointManager {
     /*
     initialize a DataPoint object with the date (month, day, year)
      */
     public DataPoint createDataPoint(int month, int day, int year) {
         return new DataPoint(month, day, year);
-    }
-
-    /*
-    delete DataPoint from the DataPointMap
-     */
-    public void deleteDataPoint(DataPoint dp, DataPointMap map) {
-        map.removeDataPoint(dp);
     }
 }
