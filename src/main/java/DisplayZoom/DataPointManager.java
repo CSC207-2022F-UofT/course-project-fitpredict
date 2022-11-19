50a293e70c438754e19b9b9ca900a37976e524bd
elsiezhu <elsie.zhu2003@gmail.com>
2022-11-18 23:33:45 -0500

    add error handling to catch invalid calendar dates when initializing DataPoint objects

--- src/main/java/DisplayZoom/DataPointManager.java
+++ src/main/java/DisplayZoom/DataPointManager.java
@@ -8,6 +8,25 @@ public class DataPointManager {
     initialize a DataPoint object with the date (month, day, year)
      */
     public DataPoint createDataPoint(int month, int day, int year) {
+        // raise IllegalArgumentException if month is invalid
+        if (month > 12 || month < 1) {
+            throw new IllegalArgumentException ("A valid calendar month must be between 1-12, inclusive");
+        }
+
+        // raise IllegalArgumentException if day is invalid
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException ("A valid calendar day must be between 1-31, inclusive");
+        } else if (day > 28 && month == 2) {
+            throw new IllegalArgumentException ("A valid calendar day in February must be between 1-28, inclusive");
+        } else if (day > 30 && (month == 4 || month == 6 || month == 9 || month == 11)) {
+            throw new IllegalArgumentException ("A valid calendar day in 30-day months must be between 1-30, inclusive");
+        }
+
+        //raise IllegalArgumentException if year is invalid
+        if (year < 1) {
+            throw new IllegalArgumentException ("A valid calendar year must be greater than or equal to 1");
+        }
+
         return new DataPoint(month, day, year);
     }
 }
