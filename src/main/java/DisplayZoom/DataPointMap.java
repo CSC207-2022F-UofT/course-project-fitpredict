4d2b23c34064efacb9925d07710578e438a97d22
elsiezhu <elsie.zhu2003@gmail.com>
2022-11-20 01:22:01 -0500

    added docstrings to DataPointMap.java

--- src/main/java/DisplayZoom/DataPointMap.java
+++ src/main/java/DisplayZoom/DataPointMap.java
@@ -2,25 +2,25 @@ package DisplayZoom;
 import java.util.*;
 import java.util.HashMap;
 
-/*
-DataPointMap class : Mapping of Date objects to DataPoint objects
+/**
+ * DataPointMap use case - Mapping of Date objects to DataPoint objects
  */
 public class DataPointMap {
-    private HashMap<Date, DataPoint> dataPointMap;
+    private HashMap<Date, DataPoint> dataPointMap = new HashMap<Date, DataPoint>();
 
-    public DataPointMap(ArrayList<DataPoint> dataPoints) {
-    }
-
-    /*
-    Getter function that returns the HashMap of Date objects mapped to their DataPoint objects
+    /**
+     * Gets the dataPointMap
+     * @return dataPointMap
      */
     public HashMap<Date, DataPoint> getData() {
         return this.dataPointMap;
     }
 
-    /*
-    Getter function that returns the DataPoint object in the DataPointMap with the corresponding Date
-    returns null if a DataPoint with that corresponding date does not exist in the DataPointMap
+    /**
+     * Gets the DataPoint object in the DataPointMap with the corresponding Date
+     * returns null if a DataPoint with that corresponding date does not exist in the DataPointMap
+     * @param date Date of DataPoint
+     * @return DataPoint
      */
     public DataPoint getDataPoint(Date date) {
         for (Date d: this.getData().keySet()) {
@@ -31,8 +31,9 @@ public class DataPointMap {
         return null; // return null if dataPoint is not found
     }
 
-    /*
-    method to merge a HashMap of Date objects mapped to DataPoint objects and a DataPointMap object
+    /**
+     * merge a HashMap of Date objects mapped to DataPoint objects and a DataPointMap object
+     * @param map HashMap to be merged with dataPointMap
      */
     public void mergeDataPoints(HashMap<Date, DataPoint> map) {
         for (Date date : map.keySet()) {
@@ -40,8 +41,9 @@ public class DataPointMap {
         }
     }
 
-    /*
-    setter function to add a DataPoint object to the DataPointMap
+    /**
+     * add a DataPoint object to the DataPointMap
+     * @param dp DataPoint to be added to DataPointMap
      */
     public void addDataPoint(DataPoint dp) {
         // if the date is already mapped to a DataPoint, then over-write that DataPoint with this new one
@@ -49,8 +51,9 @@ public class DataPointMap {
         this.dataPointMap.put(dp.getDate(), dp);
     }
 
-    /*
-    setter function to delete a DataPoint object from the DataPointMap
+    /**
+     * delete a DataPoint object from the DataPointMap
+     * @param dp DataPoint to be deleted
      */
     public void removeDataPoint(DataPoint dp) {
         if (this.getDataPoint(dp.getDate()) != null) {
