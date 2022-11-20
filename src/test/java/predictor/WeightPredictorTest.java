884f6112d6e71c99de6335e155638893bc8f8bc2
VangoCode <ronvarshavsky1@gmail.com>
2022-11-20 01:50:20 -0500

    finished test suite structure for all test files

--- src/test/java/predictor/WeightPredictorTest.java
+++ src/test/java/predictor/WeightPredictorTest.java
@@ -1,5 +1,32 @@
 package predictor;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WeightPredictorTest {
-    // TODO: create test suite for edge cases
+    @Test
+    public void WeightPredictorZero() {
+        DataPointMap data = new DataPointMap();
+        // Assert that the return type is empty
+        assertEquals(WeightPredictor.predict(data).size(), 0);
+    }
+
+    @Test
+    public void WeightPredictorNoChange() {
+        // test that the Weight predictor knows when not to change
+        // TODO: when this function is implemented, write a test case
+        // because at the moment, it cannot properly be written
+    }
+
+    @Test
+    public void WeightPredictorSmallData() {
+        // TODO: when this function is implemented, write a test case
+        // because at the moment, it cannot properly be written
+    }
+
+    @Test
+    public void WeightPredictorBigData() {
+        // TODO: when this function is implemented, write a test case
+        // because at the moment, it cannot properly be written
+    }
 }
