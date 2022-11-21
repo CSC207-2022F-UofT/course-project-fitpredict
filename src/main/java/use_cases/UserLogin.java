d5e699da207b97e55d1eb648f3fa84a0ce7e2857
selintasman <selintasman14@gmail.com>
2022-11-21 18:27:29 -0500

    A bug in UserLogin is now fixed and test case to check whether the user is added into the accountlist.

--- src/main/java/use_cases/UserLogin.java
+++ src/main/java/use_cases/UserLogin.java
@@ -36,11 +36,16 @@ public class UserLogin implements LoginInputBoundary {
      */
     public LogInResult loginUser(String username, String password) {
         User user = users.getUser(username);
-        if (!user.checkPasswordMatches(password)) {
-            System.out.println("The passwords don't match");
+        if (users.getUser(username) != null) {
+            if (!user.checkPasswordMatches(password)) {
+                System.out.println("The passwords don't match");
+                return LogInResult.NO_LOG;
+            }
+            CurrentUser.getInstance().setUser(user); // This is the user that is currently logged in.
+            return LogInResult.LOG;
+        }
+        else {
             return LogInResult.NO_LOG;
         }
-        CurrentUser.getInstance().setUser(user); // This is the user that is currently logged in.
-        return LogInResult.LOG;
     }
 }
