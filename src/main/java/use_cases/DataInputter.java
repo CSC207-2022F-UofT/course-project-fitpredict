c4e7e06afebdf53ca9ea61d0f728d75283f2b72c
aubreynadler <aubreynadler@gmail.com>
2022-11-22 21:56:47 -0500

    Creation of DataPoint now uses DataPointManager

--- src/main/java/use_cases/DataInputter.java
+++ src/main/java/use_cases/DataInputter.java
@@ -3,13 +3,14 @@ package use_cases;
 import entities.CurrentUser;
 import entities.DataPoint;
 import entities.Exercise;
-
 import java.util.ArrayList;
 
 public class DataInputter implements DataInputBoundary {
+
     public void inputData(int month, int day, int year, double weight, ArrayList<Exercise> exercises, double caloriesBurnt) {
         DataPointMap dataPointMap = CurrentUser.getInstance().getDataPointMap();
-        DataPoint dataPoint = new DataPoint(month, day, year);
+        DataPointManager manager = new DataPointManager();
+        DataPoint dataPoint = manager.createDataPoint(month, day, year);
         dataPoint.setWeight(weight);
         dataPoint.addExercise(exercises);
         dataPoint.setCaloriesBurnt(caloriesBurnt);
