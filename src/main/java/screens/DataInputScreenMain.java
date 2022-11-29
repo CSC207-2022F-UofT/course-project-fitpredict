a5ac90d60cfa5077c2ccbb4f253445bd3fa8f64c
aubreynadler <aubreynadler@gmail.com>
2022-11-29 11:52:10 -0500

    Created helper method for the method that triggers on input data button press to deal with invalid inputs. Also removed unused imports in both the screen and dataInputScreenMain

--- src/main/java/screens/DataInputScreenMain.java
+++ src/main/java/screens/DataInputScreenMain.java
@@ -3,9 +3,13 @@ package screens;
 
 import controllers.DataInputController;
 import entities.CurrentUser;
+import entities.DataPoint;
+import entities.Exercise;
 import entities.User;
 import use_cases.DataInputter;
 import use_cases.DataInputBoundary;
+import use_cases.ExerciseMap;
+
 
 public class DataInputScreenMain {
     /**
@@ -13,7 +17,12 @@ public class DataInputScreenMain {
      */
     public static void main(String[] args) {
         User sampleUser = new User("selintasman1", "Hello1234", "Hello1234", 15.0, 1.64, "Female", "2002-08-14");
-        CurrentUser.getInstance().setUser(sampleUser);
+        CurrentUser cu = CurrentUser.getInstance();
+        cu.setUser(sampleUser);
+        Exercise sampleExercise = new Exercise("Push Up", 7);
+        ExerciseMap.addExercise(cu, sampleExercise);
+        DataPoint sampleDataPoint = new DataPoint(10, 10, 2000);
+        cu.getUser().getDataPointMap().addDataPoint(sampleDataPoint);
         DataInputBoundary boundary = new DataInputter();
         DataInputController controller = new DataInputController(boundary);
         DataInputScreen screen = new DataInputScreen(controller);
