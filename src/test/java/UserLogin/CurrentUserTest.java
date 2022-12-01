0577452a7f72bd4583942a8f950f9c89668130fa
VangoCode <ronvarshavsky1@gmail.com>
2022-12-01 13:20:19 -0500

    fixed tests for CurrentUser

--- src/test/java/UserLogin/CurrentUserTest.java
+++ src/test/java/UserLogin/CurrentUserTest.java
@@ -5,14 +5,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CurrentUserTest {
-    CurrentUser user1;
+    User user1; // The user that is to be logged in
 
     @Before
     public void setUp() {
-        user1 = new CurrentUser("Selin1", "A12345", "A12345", 15.0, 1.55,
+        user1 = new User("Selin1", "A12345", "A12345", 15.0, 1.55,
                 "Female", "2002-07-14");
+        CurrentUser.getInstance().setUser(user1); // LOG IN THE USER!
     }
 
     @After
@@ -20,25 +23,27 @@ public class CurrentUserTest {
 
     @Test
     public void getUserTest() {
-        assert (user1.getUser().equals(user1));
+        assertEquals(user1, CurrentUser.getInstance().getUser());
     }
 
     @Test
     public void getUsernameTest() {
-         assert (user1.getUsername().equals("Selin1"));
+         assertEquals ("Selin1", CurrentUser.getInstance().getUser().getUsername());
     }
 
     @Test
     public void setUsernameTest() {
         String expected = "Sonat1";
-        user1.setUsername(expected);
-        assert (user1.getUsername().equals(expected));
+        CurrentUser.getInstance().getUser().setUsername(expected);
+        assertEquals(expected, CurrentUser.getInstance().getUser().getUsername());
     }
 
     @Test
     public void setUserTest() {
-        user1.setUser(user1);
-        assert (user1.getUser().equals(user1));
+        User user2 = new User("Selin2", "A12343", "A12343", 15.2, 1.52,
+                "Female", "2002-07-12");
+        CurrentUser.getInstance().setUser(user2);
+        assertEquals(user2, CurrentUser.getInstance().getUser());
     }
 
 }
