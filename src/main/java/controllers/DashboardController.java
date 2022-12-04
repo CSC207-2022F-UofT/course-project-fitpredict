72dc3afd7c589dbc41d2f04db01b31b679c8e1dd
VangoCode <ronvarshavsky1@gmail.com>
2022-12-03 23:03:11 -0500

    refactored to better fit Clean Architecture

--- src/main/java/controllers/DashboardController.java
+++ src/main/java/controllers/DashboardController.java
@@ -1,34 +1,11 @@
 package controllers;
 
-import entities.CurrentUser;
-import entities.DataPoint;
-import use_cases.DataPointManager;
-import use_cases.DataPointMap;
-import use_cases.PredictManager;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import use_cases.UpdatePredictions;
 
 public class DashboardController {
+    public DashboardController() {  }
 
     public void updateDashboardPredictions() {
-        DataPointMap data = CurrentUser.getInstance().getUser().getDataPointMap();
-        HashMap<Date, Double> weightPredictions = PredictManager.predictWeight(data);
-        double caloriePrediction = PredictManager.predictCalories(data);
-        DataPointManager dataPointManager = new DataPointManager();
-
-        DataPointMap predictionMap = new DataPointMap();
-        // loop through weightPredictions
-        for (Map.Entry<Date, Double> entry: weightPredictions.entrySet()) {
-            Date date = entry.getKey();
-
-            DataPoint tempDataPoint = dataPointManager.createDataPoint(date);
-            tempDataPoint.setWeight(weightPredictions.get(date));
-            tempDataPoint.setCaloriesBurnt(caloriePrediction);
-            predictionMap.addDataPoint(tempDataPoint);
-        }
-        data.mergeDataPoints(predictionMap.getData());
-
+        UpdatePredictions.updateDashboardPredictions();
     }
 }
